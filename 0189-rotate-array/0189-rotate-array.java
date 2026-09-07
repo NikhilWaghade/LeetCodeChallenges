3429c7c33f928ab4589b634cbccaf3159b70634c
Nikhil Waghade <166985527+NikhilWaghade@users.noreply.github.com>
2026-09-07 16:06:43 +0530

    Time: 4 ms (37.4%), Space: 268.8 MB (12.1%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,14 +1,12 @@
 class Solution {
      public void Rev(int[] nums, int start, int end ){
         while(start <= end){
-
         int temp = nums[start];
         nums[start]=nums[end];
         nums[end]=temp;
 
         start++;
-        end --;
-           
+        end --;    
         }
      }
     public void rotate(int[] nums, int k) {
@@ -16,7 +14,7 @@ class Solution {
         // if(k % n == 0){
         //     return ;
         // }
-        k= k % n;
+       k= k % n;
        Rev(nums, 0, n-1);
        Rev(nums, 0, k-1);
        Rev(nums, k, n-1);
