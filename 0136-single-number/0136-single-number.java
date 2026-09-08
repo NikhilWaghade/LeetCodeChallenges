0e55056e2e2a8baa2b036d66277fb575af385a63
Nikhil Waghade <166985527+NikhilWaghade@users.noreply.github.com>
2026-09-08 07:07:35 +0530

    Time: 1 ms (99.94%), Space: 46.7 MB (91.97%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,19 +1,32 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int num:nums){
-            if(!map.containsKey(num)){
-                map.put(num, 0);        
-            }
-            map.put(num,map.get(num)+1);
-        }
-        for(int num:nums){
-            if(map.get(num)==1){
-                return num;
-            }
-        }
-        return -1;
+        // Map<Integer,Integer> map = new HashMap<>();
+        // 2 2 1
+        //   n
+        // <2:0++ 1:++1: 2:2,  1:0:++1; 1:1>
+        // for(int num:nums){
+        //     if(!map.containsKey(num)){
+        //         map.put(num, 0);        
+        //     }
+        //     map.put(num,map.get(num)+1);
+        // }
+        // <2:0++ 1:++1: 2:2,  1:0:++1; 1:1>
+        // for(int num:nums){
+        //     if(map.get(num) ==1){
+        //         return num;
+        //     }
+        // }
+        // return -1;
 
+// second approach using XOR
+   int result =0;
+   //2 2 1
+   //0 ^2^2^1 
+   //2^2 = 0 and 0^0=0 and 0^1= 1 so print 1
+      for(int num: nums){
+        result ^=num;
+      }
+      return result;
    
     }
 }
