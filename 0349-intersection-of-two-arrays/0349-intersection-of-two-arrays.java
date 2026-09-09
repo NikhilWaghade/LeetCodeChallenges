4bfd78b60a402a9cb7cbdbb9b196bb921430ae5c
Nikhil Waghade <166985527+NikhilWaghade@users.noreply.github.com>
2026-09-09 09:56:04 +0530

    Time: 3 ms (47.32%), Space: 45.3 MB (5.98%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,28 +1,42 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-      Set<Integer> s1 = new HashSet<>();
-      Set<Integer> s2 = new HashSet<>();
-      
-//  Time: O(n + m)
-//  Space: O(n + m)
-      for(int num:nums1){
-        s1.add(num);
-      }
+        Set<Integer> s1 = new HashSet<>();
+        Set<Integer> s2 = new HashSet<>();
 
-      for(int num:nums2){
-        s2.add(num);
-      }
+        //  Time: O(n + m)
+        //  Space: O(n + m)
+        for (int num : nums1) {
+            s1.add(num);
+        }
 
-    //   intersection apply
-    int [] result = new int[s1.size()];
-    int k=0;
-      for(int num: s1){
-        if(s2.contains(num)){
-          result[k]=num;
-          k=k+1;
-        }        
-       }
-       //Array ka sirf required part copy karenga.
-    return Arrays.copyOfRange(result, 0, k);
+        for (int num : nums2) {
+            s2.add(num);
+        }
+
+        //   intersection apply
+        if (s1.size() < s2.size()) {
+            int[] result = new int[s1.size()];
+            int k = 0;
+            for (int num : s1) {
+                if (s2.contains(num)) {
+                    result[k] = num;
+                    k = k + 1;
+                }
+            }
+            //Array ka sirf required part copy karenga.
+            return Arrays.copyOfRange(result, 0, k);
+        } else {
+            int[] result = new int[s2.size()];
+            int k = 0;
+            for (int num : s2) {
+                if (s1.contains(num)) {
+                    result[k] = num;
+                    k = k + 1;
+                }
+            }
+            //Array ka sirf required part copy karenga.
+            return Arrays.copyOfRange(result, 0, k);
+        }
+
     }
 }
