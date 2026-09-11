ea8023c6f49f777aa209d2f75a54998b43d615de
Nikhil Waghade <166985527+NikhilWaghade@users.noreply.github.com>
2026-09-11 07:53:42 +0530

    Time: 2 ms (91.76%), Space: 48 MB (20.17%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,30 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int k=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] != 0){
-                nums[k]=nums[i];
+
+        //     int k=0;
+        // T:0(n) , S:0(1)
+        //     for(int i=0;i<nums.length;i++){
+        //         if(nums[i] != 0){
+        //             nums[k]=nums[i];
+        //             k++;
+        //           }
+        //     }
+        //    while(k < nums.length){
+        //     nums[k] = 0;
+        //     k++;
+        //    }
+
+        // second approach 
+        // Complexity
+        //   T:O(n),S:O(1)
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = 0;
+                nums[k] = temp;
                 k++;
-              }
+            }
         }
-       while(k < nums.length){
-        nums[k] = 0;
-        k++;
-       }
     }
 }
