69143629c4dd1b362a68cd0d289f13a2578a18dd
Nikhil Waghade <166985527+NikhilWaghade@users.noreply.github.com>
2026-09-14 13:04:39 +0530

    Time: 0 ms (100%), Space: 43.7 MB (70.74%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -2,21 +2,45 @@ class Solution {
     public void rotate(int[][] matrix) {
         // transpose
         // T: 0(n^2) , S: 0(1)
-        int n = matrix.length;
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
-        }
+        // int n = matrix.length;
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         int temp = matrix[i][j];
+        //         matrix[i][j] = matrix[j][i];
+        //         matrix[j][i] = temp;
+        //     }
+        // }
 
-        // row reverse 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n/2; j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n-1-j];
-                matrix[i][n-1-j] = temp;
+        // // row reverse 
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<n/2; j++){
+        //         int temp = matrix[i][j];
+        //         matrix[i][j] = matrix[i][n-1-j];
+        //         matrix[i][n-1-j] = temp;
+        //     }
+        // }
+
+        // second optimal approach
+        int n = matrix.length;
+        for(int i=0;i<n/2;i++){
+            for(int j=i; j<n-i-1; j++){
+                // cycle 
+                int delta = j-i;
+
+                int curr = matrix[i][j];
+                int next = matrix[i + delta][n-i-1];
+                matrix[i + delta][n-i-1] = curr;
+                curr = next ;
+
+                next = matrix[n-i-1] [n-i-1-delta];
+                matrix[n-1-i] [n-i-1-delta] = curr;
+                curr = next;
+
+                next = matrix[n-i-1-delta][i];
+                matrix[n-i-1-delta][i] = curr;
+                curr = next ;
+
+                matrix[i][j] = curr;
             }
         }
     }
