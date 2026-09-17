22494b1bdddb8943a6cab36fd6d1a2647e4cc252
Nikhil Waghade <166985527+NikhilWaghade@users.noreply.github.com>
2026-09-17 13:23:33 +0530

    Time: 12 ms (28.28%), Space: 46.8 MB (61.87%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,31 +1,44 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-       int [] desti = new int [arr.length];
-       int sIdx = 0;
-       int dIdx =0;
+        //    int [] desti = new int [arr.length];
+        //    int sIdx = 0;
+        //    int dIdx =0;
 
-    //    T:o(n) , S: o(n)
-       while(sIdx < arr.length) {
-        if(arr[sIdx] == 0) {
-            if(dIdx < arr.length) {
-                desti[dIdx] = 0;
-            }
-            dIdx++;
-            if(dIdx < arr.length) {
-                desti[dIdx] = 0;
+        // //    T:o(n) , S: o(n) Brute Forch Approach
+        //    while(sIdx < arr.length) {
+        //     if(arr[sIdx] == 0) {
+        //         if(dIdx < arr.length) {
+        //             desti[dIdx] = 0;
+        //         }
+        //         dIdx++;
+        //         if(dIdx < arr.length) {
+        //             desti[dIdx] = 0;
+        //         }
+        //     }
+        //     else{
+        //         if(dIdx < arr.length) {
+        //             desti[dIdx] = arr[sIdx];
+        //         }
+        //     }
+        //     dIdx++;
+        //     sIdx++;
+        //    }
+        //    for(int i=0; i<a{rr.length;i++){
+        //     arr[i] = desti[i];
+        //    }
+
+        // second approach Bruth foarch 2
+
+        int n = arr.length;
+        // T:0(n^2), S:0()
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                for (int j = n - 1; j > i; j--) {
+                    arr[j] = arr[j - 1];
+                }
+                i++;
             }
         }
-        else{
-            if(dIdx < arr.length) {
-                desti[dIdx] = arr[sIdx];
-            }
-        }
-        dIdx++;
-        sIdx++;
-       }
-       for(int i=0; i<arr.length;i++){
-        arr[i] = desti[i];
-       }
-        
+
     }
 }
