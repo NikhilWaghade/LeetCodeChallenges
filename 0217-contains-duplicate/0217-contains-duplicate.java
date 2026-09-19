e8dceabd177096e0b2373d1748df8ceb0f3c4dde
Nikhil Waghade <166985527+NikhilWaghade@users.noreply.github.com>
2026-09-19 11:58:18 +0530

    Time: 17 ms (88.26%), Space: 108.4 MB (23.13%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,23 +1,36 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        // 1. nested method T:0(n^2), S:0(1)
         // for(int i=0; i<nums.length;i++){
-        //     if(nums[i] != nums[i+1]){
-        //         return false;
-        //     }
-        //     else{
-        //         return true;
+        //     for(int j=i+1; j<nums.length;j++){
+        //         if(nums[i] == nums[j]){
+        //             return true;
+        //         }
         //     }
         // }
         // return false;
 
-    //   sorting method 
-       Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            // T:O(n log n), S:O(log n)
-            if(nums[i] == nums[i+1]){
-                return true;
-            }
+    //   2. sorting method 
+    //    Arrays.sort(nums);
+    //     for(int i=0;i<nums.length-1;i++){
+    //         // T:O(n log n), S:O(log n)
+    //         if(nums[i] == nums[i+1]){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+
+    // 3. HashSet method 
+    Set<Integer> set = new HashSet<>();
+
+    for(int num : nums){
+        // T:0(n), S:o(n)
+        if(set.contains(num)){
+            // contains = Check if exists
+            return true;
         }
-        return false;
+        set.add(num); //Add element to set
+    }
+     return false;
     }
 }
