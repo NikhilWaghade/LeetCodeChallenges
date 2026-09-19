d6a1018d79adbb6a417c7aa9d09b396fcae39a32
Nikhil Waghade <166985527+NikhilWaghade@users.noreply.github.com>
2026-09-19 15:54:45 +0530

    Time: 13 ms (28.24%), Space: 46.6 MB (15.87%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,61 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        //1. Sorting Method
+        //   T:o(n log n), S:o(n)
+
+        // char[] a = s.toCharArray();
+        // char[] b = t.toCharArray();
+
+        // Arrays.sort(a);
+        // Arrays.sort(b);
+
+        // return Arrays.equals(a, b);
+
+        // 2. optimal approach
+        // T:o(n), S:o(1)
+        // if(s.length() != t.length()) {
+        //     return false ;
+        // }
+
+        // int [] freq = new int [26];
+
+        // for(char c : s.toCharArray()) {
+        //     int idx = c - 'a';
+        //     freq[idx] = freq[idx] + 1;
+        // }   
+
+        // for(char c : t.toCharArray()){
+        //     int idx = c - 'a';
+        //     freq[idx] = freq[idx] - 1;
+        // }
+
+        // for(int i =0; i<26; i++){
+        //     if(freq[i] != 0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        // 3. HashMap Method 
+        // T: o(n), S:o(n)
+        if(s.length() != t.length()) {
+            return false;
+        }
+        HashMap <Character , Integer> freq = new HashMap<>();
+
+        for(char c : s.toCharArray()){
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c : t.toCharArray()){
+            freq.put(c, freq.getOrDefault(c, 0) - 1);
+        }
+
+        for(char c : freq.keySet()){
+            if(freq.get(c) != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
