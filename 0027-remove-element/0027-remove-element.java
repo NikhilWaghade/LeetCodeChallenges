e00bac639e46bbedae5abf6e31e01043a9af262d
Nikhil Waghade <166985527+NikhilWaghade@users.noreply.github.com>
2026-09-23 06:39:59 +0530

    Time: 0 ms (100%), Space: 43.4 MB (78.75%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,14 +1,28 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        // approch 1 
+        // approch 1 Order Maintained
         // T:o(n), o(1)
+        // int i =0;
+        // for(int j=0; j<nums.length;j++){
+        //     if(nums[j] != val){
+        //         nums[i] = nums[j];
+        //         i++;
+        //     }
+        // }
+        // return i;
+
+        // approch 2 Order Not Maintained but Good
+        int n = nums.length;
         int i =0;
-        for(int j=0; j<nums.length;j++){
-            if(nums[j] != val){
-                nums[i] = nums[j];
+        while(i<n){
+            if(nums[i] == val){
+                nums[i] = nums[n-1];
+                n--;
+            }
+            else{
                 i++;
             }
         }
-        return i;
+        return n;
     }
 }
